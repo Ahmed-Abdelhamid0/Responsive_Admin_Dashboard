d6bdc22b07102afdae155e8367ba7320ce9cab8c
༺𝒜𝒽𝓂𝑒𝒹༻ <aabdulhamed363@gmail.com>
2024-09-15 13:51:56 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
+import 'package:admin_dashboard/views/responsive_dashboard_view.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main()=>runApp(const ResponsiveDashboard());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ResponsiveDashboard extends StatelessWidget {
+  const ResponsiveDashboard({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Placeholder(),
+  Widget build(BuildContext context) => const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DashboardView(),
     );
-  }
+
 }
 
 
