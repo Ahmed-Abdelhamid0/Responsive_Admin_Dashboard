dde679967dbb2e8ec671ac9f5daf140c21e97d1e
Ahmed-Abdelhamid0 <aabdulhamed363@gmail.com>
2024-09-15 17:17:16 +0300

    first commit

--- lib/views/widgets/adabtive_layout.dart
+++ lib/views/widgets/adabtive_layout.dart
@@ -11,7 +11,7 @@ class AdaptiveLayout extends StatelessWidget {
         builder: (context,constrains){
           if(constrains.maxWidth < 600){
             return mobileLayout(context);
-          }else if(constrains.maxWidth < 900){
+          }else if(constrains.maxWidth < 1200){
             return tabletLayout(context);
           }else{
             return desktopLayout(context);
