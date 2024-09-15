99e42fcab8e9d102310b68ddd3e460b5b5bf35fa
Ahmed-Abdelhamid0 <aabdulhamed363@gmail.com>
2024-09-15 16:59:15 +0300

    first commit

---
+++ lib/models/income_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class IncomeModel{
+  final Color color;
+  final String title;
+  final String percentage;
+
+  const IncomeModel({required this.color, required this.title, required this.percentage});
+}
