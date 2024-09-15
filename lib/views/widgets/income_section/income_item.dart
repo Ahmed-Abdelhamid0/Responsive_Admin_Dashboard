3b9e04114229c9ba9367fcdef53cf779d5243ef7
Ahmed-Abdelhamid0 <aabdulhamed363@gmail.com>
2024-09-15 17:00:05 +0300

    first commit

---
+++ lib/views/widgets/income_section/income_item.dart
@@ -0,0 +1,29 @@
+import 'package:admin_dashboard/models/income_model.dart';
+import 'package:admin_dashboard/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class IncomeItem extends StatelessWidget {
+  const IncomeItem({super.key, required this.incomeModel});
+final IncomeModel incomeModel;
+  @override
+  Widget build(BuildContext context) {
+    return  ListTile(
+      leading: Container(
+        width: 12,
+        height: 12,
+        color:incomeModel.color
+      ),
+      title: Text(
+        incomeModel.title,
+        style: AppStyles.styleRegular16,
+      ),
+      trailing: Text(
+        incomeModel.percentage,
+        style: AppStyles.styleMedium16.copyWith(
+          color: const Color(0xff208CC8),
+        ),
+      ),
+
+    );
+  }
+}
