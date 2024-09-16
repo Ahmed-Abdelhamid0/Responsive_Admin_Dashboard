eb2063a73589a543108935c54da6c8a07f4b367b
Ahmed-Abdelhamid0 <aabdulhamed363@gmail.com>
2024-09-16 14:16:02 +0300

    first commit

--- lib/views/widgets/all_expenses_section/all_expenses.dart
+++ lib/views/widgets/all_expenses_section/all_expenses.dart
@@ -1,6 +1,6 @@
 import 'package:admin_dashboard/views/widgets/all_expenses_section/all_expenses_header.dart';
 import 'package:admin_dashboard/views/widgets/all_expenses_section/all_expenses_list_view.dart';
-import 'package:admin_dashboard/views/widgets/custom_background_container.dart';
+import 'package:admin_dashboard/utils/custom_background_container.dart';
 import 'package:flutter/material.dart';
 
 class AllExpense extends StatelessWidget {
