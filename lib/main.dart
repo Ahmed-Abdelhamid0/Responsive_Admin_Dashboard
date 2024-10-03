956e47efcb100f9885782d2ae2c0082f02f0f64f
Ahmed-Abdelhamid0 <aabdulhamed363@gmail.com>
2024-10-03 11:54:34 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:admin_dashboard/views/responsive_dashboard_view.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
-void main()=>runApp(const ResponsiveDashboard());
+void main() {
+  runApp(DevicePreview(
+      enabled: true, builder: (context) => const ResponsiveDashboard()));
+}
 
 class ResponsiveDashboard extends StatelessWidget {
   const ResponsiveDashboard({super.key});
 
   @override
   Widget build(BuildContext context) => const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DashboardView(),
-    );
-
+        debugShowCheckedModeBanner: false,
+        builder: DevicePreview.appBuilder,
+        home: DashboardView(),
+      );
 }
-
-
