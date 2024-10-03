1e1363c1a1445aca18e10e3a44cf9e63f52d0921
Ahmed-Abdelhamid0 <aabdulhamed363@gmail.com>
2024-10-03 14:02:54 +0300

    first commit

--- lib/views/widgets/drawer_section/disactive_drawer_item.dart
+++ lib/views/widgets/drawer_section/disactive_drawer_item.dart
@@ -15,9 +15,13 @@ class DisActiveItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       leading: SvgPicture.asset(drawerItemModel.image),
-      title: Text(
-        drawerItemModel.title,
-        style: AppStyles.styleMedium16(context),
+      title: FittedBox(
+        alignment: Alignment.centerLeft,
+        fit: BoxFit.scaleDown,
+        child: Text(
+          drawerItemModel.title,
+          style: AppStyles.styleMedium16(context),
+        ),
       ),
     );
   }
